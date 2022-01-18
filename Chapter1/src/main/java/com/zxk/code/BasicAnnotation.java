de4e09ab360079d1728e5f9631841f6794b821f8
zhangxiaokang <skongcheung@163.com>
2022-01-18 21:38:50 +0800

    Changes

--- Chapter1/src/main/java/com/zxk/code/BasicAnnotation.java
+++ Chapter1/src/main/java/com/zxk/code/BasicAnnotation.java
@@ -1,10 +1,46 @@
 package com.zxk.code;
 
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 public class BasicAnnotation {
     @Test
     public void testCase1(){
-        System.out.println("这是测试用例1");
+        System.out.println("testCase1这是测试用例1");
+    }
+
+    @Test
+    public void testCase2(){
+        System.out.println("testCase1这是测试用例2");
+    }
+
+    @BeforeMethod
+    public void beforeMethod(){
+        System.out.println("beforeMethod这是在测试方法之前运行的");
+    }
+
+    @AfterMethod
+    public void afterMethod(){
+        System.out.println("afterMethod这是在测试方法之后运行的");
+
+    }
+
+    @BeforeClass
+    public void beforeClass(){
+        System.out.println("beforeClass这是在类运行之前运行的方法");
+    }
+
+    @AfterClass
+    public void afterClass(){
+        System.out.println("afterClass这是在类运行之后运行的方法");
+    }
+
+    @BeforeSuite
+    public void beforeSuite(){
+        System.out.println("beforeSuite测试套件");
+    }
+
+    @AfterSuite
+    public void afterSuite(){
+        System.out.println("afterSuite测试套件");
     }
 }
